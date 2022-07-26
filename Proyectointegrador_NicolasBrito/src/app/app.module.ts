65d112bb3d785896f97c417dd7d2b14c6efb02ab
nikochittoo <nicolasbrito1670@gmail.com>
2022-07-26 16:34:48 -0300

    experiencia & school goood terminado, (6,11:43) h&s

--- Proyectointegrador_NicolasBrito/src/app/app.module.ts
+++ Proyectointegrador_NicolasBrito/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +21,14 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
     SocialComponent,
     BannerComponent,
     AcercaDeComponent,
-    ExperienciaComponent
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
